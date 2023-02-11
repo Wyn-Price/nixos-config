5ec16d008266e17478353884dbff3b2ff76f6435
Wyn Price <github@wynprice.com>
2023-02-11 19:34:39 +0000

    Add home manager search

--- home/firefox.nix
+++ home/firefox.nix
@@ -5,8 +5,22 @@
     profiles.wp = {
       name = "Wyn Price";
       search.default = "Google";
+      
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         onepassword-password-manager
+      ];
+
+      bookmarks = [
+        {
+          name = "Search";
+          bookmarks = [
+            {
+              name = "Home Manager";
+              keyword = ":hm";
+              url = "https://mipmip.github.io/home-manager-option-search/?%s";
+            }
+          ];
+        }
       ];
     };
   };
