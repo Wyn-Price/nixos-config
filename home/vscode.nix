6c734b2bd9e0c5610841cbed2c8320cc5352d32a
Wyn Price <github@wynprice.com>
2023-02-13 21:28:51 +0000

    Add vscode config

--- home/vscode.nix
+++ home/vscode.nix
@@ -2,10 +2,13 @@
 {
   programs.vscode = {
     enable = true;
+
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
       bbenoist.nix
       shardulm94.trailing-spaces
     ];
+    keybindings = with builtins; fromJSON (readFile ./data/vscode/keybinds.json);
+    userSettings = with builtins; fromJSON (readFile ./data/vscode/settings.json);
   };
 }
