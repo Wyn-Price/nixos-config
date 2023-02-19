07123f69ab6cfeec1fa40229a349cce21465a9f6
Wyn Price <github@wynprice.com>
2023-02-19 01:09:37 +0000

    Configure lightdm login page

--- shared/i3.nix
+++ shared/i3.nix
@@ -9,6 +9,19 @@
     displayManager = {
       defaultSession = lib.mkForce "none+i3";
     };
+    displayManager.lightdm = {
+      background = "#080808";
+      greeters.mini =  {
+        enable = true;
+        user = "wp";
+        extraConfig = ''
+          window-color = "#1B1D1E"
+          text-color = "#F8F8F0"
+          password-border-width = 0px
+          font-weight = normal
+        '';
+      };
+    };
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
