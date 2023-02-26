e3a3d23057c1eea736b24fafd9d323a5d507233d
Wyn Price <github@wynprice.com>
2023-02-26 16:15:06 +0000

    Move back to i3lock and move to i3status-rs

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -26,6 +26,12 @@
         "${modifier}+Shift+${up}" = "move up";
         "${modifier}+Shift+${down}" = "move down";
       };
+      bars = [
+        {
+          position = "bottom";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs";
+        }
+      ];
     };
     extraConfig = ''
       exec ${pkgs.xautolock}/bin/xautolock \
@@ -33,20 +39,19 @@
            -corners -000 \
            -notify 30 \
            -notifier "${pkgs.libnotify}/bin/notify-send -u critical -t 30000 -- 'LOCKING screen in 30 seconds'" \
-           -locker '${pkgs.lightdm}/bin/dm-tool lock' \
-           -detectsleep
+           -locker '${pkgs.i3lock}/bin/i3lock -c 450041 -e' \
+           -detectsleep \
+           &
     '';
   };
 
   home.packages = with pkgs; [
+    i3lock
     xautolock
   ];
 
-  programs.i3status = {
+  programs.i3status-rust = {
     enable = true;
-    general = {
-      interval = 1;
-    };
   };
 
 }
