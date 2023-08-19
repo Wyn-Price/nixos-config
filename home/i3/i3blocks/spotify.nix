6b8a31449318cee1142c90a4562f31936c1eacc6
Wyn Price <github@wynprice.com>
2023-08-19 16:26:24 +0100

    Remove bpm

--- home/i3/i3blocks/spotify.nix
+++ home/i3/i3blocks/spotify.nix
@@ -6,10 +6,11 @@ command=/home/wp/programming/i3status-rs-spotify/target/debug/i3status-rs-spotif
 interval=persist
 format=json
 
-[spotify-bpm]
-command=/home/wp/programming/i3status-rs-spotify/target/debug/i3status-rs-spotify bpm
-interval=persist
-format=json
+## Broken at some point - reqwest isn't able to query the API?
+# [spotify-bpm]
+# command=/home/wp/programming/i3status-rs-spotify/target/debug/i3status-rs-spotify bpm
+# interval=persist
+# format=json
 
 [spotify-progress]
 command=/home/wp/programming/i3status-rs-spotify/target/debug/i3status-rs-spotify progress
