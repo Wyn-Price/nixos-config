cca9ea32b4b02359330973eb4bf8407c0d0d142f
Wyn Price <github@wynprice.com>
2023-11-16 15:38:11 +0000

    Add home and home.proxy to ssh config

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -32,6 +32,31 @@
         };
       };
 
+      home = {
+        hostname = "home.wynprice.com";
+        forwardAgent = true;
+        remoteForwards = [
+          {
+            bind.address = "/run/user/1000/gnupg/S.gpg-agent";
+            host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
+          }
+        ];
+        extraOptions = {
+          StreamLocalBindUnlink = "yes";
+        };
+      };
+
+      "home.proxy" = {
+        hostname = "home.wynprice.com";
+        localForwards = [
+          {
+            bind.port = 1080;
+            host.address = "127.0.0.1";
+            host.port = 1080;
+          }
+        ];
+      };
+
     };
 
   };
